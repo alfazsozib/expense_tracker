cc461c9c2b374a124e2234494d3cae46c251a367
safinelahi <safinelahi@gmail.com>
2025-04-11 02:44:18 +0600

    signUp page done

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,17 +1,20 @@
 import React from "react";
+import Image_1 from "../../assets/1.png";
 
 const SignUp = () => {
   return (
     <div>
-      <div className="flex ">
+      <div className="flex w-full">
         {/* Left Side - Signup Form */}
-        <div className="w-[720px] h-[1024px] px[137px] pt-[150px] flex flex-col justify-center items-center bg-white p-10">
+        <div className="w-[60%]  px[137px] pt-[150px] flex flex-col justify-center items-center bg-white p-10">
           <h1 className="text-4xl font-freehand mb-10">Welcome</h1>
 
           <div className="w-full max-w-md">
             <div className="flex gap-4 mb-4">
               <div className="flex flex-col w-1/2">
-                <label className="text-[20px] font-normal text-[#000] mb-1">First Name</label>
+                <label className="text-[20px] font-normal text-[#000] mb-1">
+                  First Name
+                </label>
                 <input
                   type="text"
                   name="f_Name"
@@ -19,7 +22,9 @@ const SignUp = () => {
                 />
               </div>
               <div className="flex flex-col w-1/2">
-                <label className="text-[20px] font-normal text-[#000] mb-1">Last Name</label>
+                <label className="text-[20px] font-normal text-[#000] mb-1">
+                  Last Name
+                </label>
                 <input
                   type="text"
                   name="l_Name"
@@ -29,7 +34,9 @@ const SignUp = () => {
             </div>
 
             <div className="flex flex-col mb-4">
-              <label className="text-[20px] font-normal text-[#000] mb-1 ">Email</label>
+              <label className="text-[20px] font-normal text-[#000] mb-1 ">
+                Email
+              </label>
               <input
                 type="email"
                 name="email"
@@ -38,7 +45,9 @@ const SignUp = () => {
             </div>
 
             <div className="flex flex-col mb-6">
-              <label className="text-[20px] font-normal text-[#000] mb-1">Password</label>
+              <label className="text-[20px] font-normal text-[#000] mb-1">
+                Password
+              </label>
               <input
                 type="password"
                 name="password"
@@ -48,7 +57,8 @@ const SignUp = () => {
 
             <button
               name="submit"
-              className="w-full bg-sky-300 text-[#000000bf] rounded mb-4 text-xl py-3 ">
+              className="w-full bg-sky-300 text-[#000000bf] rounded mb-4 text-xl py-3 "
+            >
               Create Account
             </button>
 
@@ -66,8 +76,14 @@ const SignUp = () => {
         </div>
 
         {/* Right Side - Dashboard Preview */}
-        <div>
-          <img src="" alt="" />
+        <div className="w-[40%] bg-[#CAF0F8] flex justify-end items-center rounded-l-[10px]">
+          <div className="bg-white w-[620px] h-[807px] rounded flex justify-end items-center">
+            <img
+              src={Image_1}
+              alt="Dashboard Preview"
+              className="max-w-full h-auto"
+            />
+          </div>
         </div>
       </div>
     </div>
